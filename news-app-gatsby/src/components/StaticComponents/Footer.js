b87b759d47657db24d47d0eeba95c9bf18c174c4
magbee <david.magbee@me.com>
2020-01-10 12:18:34 -0500

    additional css adjustments and content fill before deployment

--- news-app-gatsby/src/components/StaticComponents/Footer.js
+++ news-app-gatsby/src/components/StaticComponents/Footer.js
@@ -4,20 +4,38 @@ import React from "react"
 
 const Footer = () => (
     <footer>
-        <p>This will be a Footer.</p>
+        <div className="contact">
+        <a href="mailto:david.magbee@me.com? subject=Let's Connect">Email</a>
+        <a href="https://www.linkedin.com/in/davidmagbee/">LinkedIn</a>
+        <a href="https://github.com/macuser413/">GitHub</a>
+        <a href="https://betterradio.airtime.pro/">Better Radio</a>
+        <a href="http://www.betterfarm.org/">Better Farm</a>
+        </div>
     <style>{`
       @import url("https://fonts.googleapis.com/css?family=News+Cycle&display=swap");
       footer {
         font-family: "News Cycle", sans-serif;
-        background-color: #fff;
-        height: 100px;
-        position: relative;
-        // margin-top: -100%;
-        padding-bottom: 40px;
+        background-color: #327b8c;
+        height: 120px;
+        display: flex;
+        flex-direction: column;
+        flex: 1 0 auto;
+        flex-shrink: 0;
+        align-content: center;
       }
       img {
         width: 100%;
         height: 100%;
+      }
+      a {
+        color: #0d304b;
+        text-decoration: none; 
+      }
+      .contact {
+          display: flex;
+          justify-content: space-around;
+          align-items: baseline;
+          align-content: center;
       }
     `}</style>
     </footer>
--- news-app-gatsby/src/pages/About.js
+++ news-app-gatsby/src/pages/About.js
@@ -12,11 +12,24 @@ const AboutPage = () => (
         In our current times where the news cycle is 24 hours long, and most of us are "connected" in some way to that cycle
         at the ring of a notification, an app icon counting what we've missed, or our incessant need to just KNOW... we need the right tools.
         <br></br>
-        This site is designed to be a resource for you to filter through the madness of the news cycle and 
+        <br></br>
+        This site is designed to be a resource for you to filter through the madness of the news cycle. 
+        It is powered by the NewsAPI, and it is a continual work in progress. 
+        If you have any requests for features, want to provide friendly, constructive feedback, or just want to say hello... please feel free to reach out to the developer. 
       </p>
       </div>
       <h1 className="about-dev">About the Developer</h1>
-      <p className="about-dev">This guy...</p>
+      <p className="about-dev">
+        Hello! I'm David Magbee, a budding developer, and I'm thrilled to have the opportunity 
+        to share a bit about myself with you on this website I've built using ReactJS with a Gatsby library. 
+        <br></br>
+        <br></br>
+        I am currently enrolled in General Assembly's Software Engineering Immersive, and it's been a great experience. 
+        As I continue to build up my skills and implement new languages and frameworks, this and other builds will evolve.
+        I look forward to sharing all of my projects with you, and I welcome a connection to discuss new features, collaboration,
+        coding tips and tricks, or just to connect and say hello. 
+        </p>
+      
     </Layout>
     <style>{`
       body {
@@ -26,6 +39,7 @@ const AboutPage = () => (
       .about-app {
         font-family: "News Cycle", sans-serif;
         margin: 15px:
+        max-width: 90%;
       }
       .about-dev {
         font-family: "News Cycle", sans-serif;
